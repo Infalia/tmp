55e0c0ea8124f3fd6eb9b561bf130e16133bd98c
Alex Mokkas <alex-mokkas@users.noreply.github.com>
2017-05-02 16:34:17 +0300

    Dummy data generation via Database Seeding

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,65 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+
+        // Users
+        DB::table('users')->insert([
+            'name' => str_random(10),
+            'email' => mb_convert_case(str_random(10), MB_CASE_LOWER, "UTF-8").'@example.com',
+            'password' => bcrypt('secret'),
+        ]);
+
+
+        // Accessibility categories
+        for($i=0; $i<4; $i++) {
+            DB::table('accessblty_cats')->insert([
+                'id' => ($i+1),
+            ]);
+        }
+
+
+        // Accessibility categories translations
+        for($i=0; $i<4; $i++) {
+            DB::table('accessblty_cat_translations')->insert([
+                'accessblty_cat_id' => ($i+1),
+                'name' => mb_convert_case(str_random(10), MB_CASE_TITLE, "UTF-8"),
+                'locale' => 'en',
+            ]);
+        }
+        for($i=0; $i<4; $i++) {
+            DB::table('accessblty_cat_translations')->insert([
+                'accessblty_cat_id' => ($i+1),
+                'name' => mb_convert_case(str_random(10), MB_CASE_TITLE, "UTF-8"),
+                'locale' => 'el',
+            ]);
+        }
+
+
+        // Accessibility options
+        for($i=0; $i<4; $i++) {
+            for($j=0; $j<4; $j++) {
+                DB::table('accessblty_opts')->insert([
+                    'accessblty_cat_id' => ($i+1),
+                ]);
+            }
+        }
+
+
+        // Accessibility options translations
+        for($i=0; $i<16; $i++) {
+            DB::table('accessblty_opt_translations')->insert([
+                'accessblty_opt_id' => ($i+1),
+                'name' => mb_convert_case(str_random(10), MB_CASE_TITLE, "UTF-8"),
+                'locale' => 'en',
+            ]);
+        }
+        for($i=0; $i<16; $i++) {
+            DB::table('accessblty_opt_translations')->insert([
+                'accessblty_opt_id' => ($i+1),
+                'name' => mb_convert_case(str_random(10), MB_CASE_TITLE, "UTF-8"),
+                'locale' => 'el',
+            ]);
+        }
     }
 }
