e20c588ef4c7239d6709557633f1cece93fab014
Alex Mokkas <alex-mokkas@users.noreply.github.com>
2017-07-04 13:05:23 +0300

    style fixes

--- resources/views/initiatives/initiatives.blade.php
+++ resources/views/initiatives/initiatives.blade.php
@@ -38,8 +38,8 @@
                             </div>
 
                             <div class="card-action card-action-footer">
-                                <span><i class="material-icons inline-icon grey-text text-darken-3">comment</i> {{ $comments = 2 }} {{ str_plural($commentLbl, $comments) }}</span>
-                                <span><i class="material-icons inline-icon grey-text text-darken-3">people</i> {{ $supporters = $initiative->users->count() }} {{ str_plural($supportLbl, $supporters) }}</span>
+                                <span class="initiative-engagement"><i class="material-icons inline-icon grey-text text-darken-3">comment</i> {{ $comments = 2 }} {{ str_plural($commentLbl, $comments) }}</span>
+                                <span class="initiative-engagement"><i class="material-icons inline-icon grey-text text-darken-3">people</i> {{ $supporters = $initiative->users->count() }} {{ str_plural($supportLbl, $supporters) }}</span>
                             </div>
                         </div>
                     </div>
